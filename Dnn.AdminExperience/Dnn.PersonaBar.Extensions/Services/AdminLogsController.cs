4d4d66f0ebc2392b287204adeb18d143da2bcb53
Mahmoud Nouraei <m.nouraei@outlook.com>
2025-04-12 08:43:46 +0330

    اعمال تغییرات برای راستچین سازی default.rtl.css

--- Dnn.AdminExperience/Dnn.PersonaBar.Extensions/Services/AdminLogsController.cs
+++ Dnn.AdminExperience/Dnn.PersonaBar.Extensions/Services/AdminLogsController.cs
@@ -100,7 +100,7 @@ namespace Dnn.PersonaBar.AdminLogs.Services
                     v.LogPortalName,
                     /* START Persian-DnnSoftware */
                     /* LogCreateDate = v.LogCreateDate.ToString("G", CultureInfo.InvariantCulture), */
-                    LogCreateDate = this.PortalSettings.CultureCode == "fa-IR" ? v.LogCreateDate.ToString("G", DotNetNuke.Services.Localization.Persian.PersianController.NewCultureInfo("fa-IR")) : v.LogCreateDate.ToString("G", CultureInfo.InvariantCulture),
+                    LogCreateDate = this.PortalSettings.CultureCode == "fa-IR" ? v.LogCreateDate.ToString("G", DotNetNuke.Services.Localization.Persian.PersianController.GetPersianCultureInfo()) : v.LogCreateDate.ToString("G", CultureInfo.InvariantCulture),
                     /* END Persian-DnnSoftware */
                     v.LogProperties.Summary,
                     LogProperties = this.controller.GetPropertiesText(v),
